586c2d285b1f4a7ba158bd2976ed58f348ff9d76
Raquel <raquelmartiniano14@gmail.com>
2025-09-30 00:31:39 +0000

    mais um exercicio feito

--- Atividade 1/ex07.cpp
+++ Atividade 1/ex07.cpp
@@ -1,13 +1,24 @@
 #include <iostream>
 #include <cmath>
 #include <iomanip>
+#include <algorithm>
 
 bool primo_operacao(int a, int b){
-    
+    bool primos = true; //tem que ser tudo minusculo
+    int n = std::min(a , b); //precisa declarar std para o programa achar o código
     for (int i = 2; i <= n;i++){
-        if (a % i == 0){
-            i
+        if (a % i == 0 && b % i == 0){ //&& é como and
+            primos = false;
         }
     }
-    return primos
+    return primos;
+}
+
+int main(){
+    int a, b;
+    std::cin >> a >> b;
+    
+    bool p = primo_operacao(a, b);
+    std::cout << p << std::endl;
+
 }
---
+++ Atividade 1/ex08.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+
+int mmc(int a, int b){
+    dividir = a / b;
+    resto = a % b;
+    while (resto != 0){
+        dividir = b / resto;
+        resto = b % resto;
+       
+    }
+}
+
+int main(){
+    int a, b;
+    std::cin >> a >> b >> std::endl;
+
+    std:: cout <<  std::endl;
+}
