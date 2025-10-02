f79b019322ca05009bcae238c13062badbb80a1c
Raquel <raquelmartiniano14@gmail.com>
2025-10-02 12:54:12 +0000

    mais uma resolvida

--- Atividade 2 (da última lista)/ex04.cpp
+++ Atividade 2 (da última lista)/ex04.cpp
@@ -26,10 +26,50 @@ int main() {
         std::cout << n << " is not prime" << std::endl;
 
     auto dur = end - beg; // Duração do cronômetro
-    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(dur);
-    std::cerr << n << " Processing time: " 
-              << duration.count() << " microseconds(s)" << std::endl;
-
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(dur);
+    std::cerr << n << " Processing time: "
+              << duration.count() << " milliseconds(s)"<< std::endl;
     return 0;
 
-}  //não está finalizado
+}  
+
+//respostas:
+//0
+//546
+//1054
+//1577
+//2104
+//2657
+//3196
+//3759
+//4239
+//4807
+//5342
+//5868
+//6429
+//6923
+//7453
+//7977
+//8548
+//9087
+//9575
+//10071
+//10632
+//11162
+//11709
+//12206
+//13289
+//13769
+//14280
+//15018
+//15389
+//15953
+//16430
+//17025
+//17546
+//18031
+//18514
+//19121
+//19666
+//20191
+//20691
