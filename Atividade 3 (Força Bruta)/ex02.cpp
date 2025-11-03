96ee2818261499a12984ee17f27f8b665096aad8
Raquel <raquelmartiniano14@gmail.com>
2025-11-03 02:24:42 +0000

    mais um exercicio

---
+++ Atividade 3 (Força Bruta)/ex02.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+
+int main() {
+    int n;
+    std::cin >> n;
+
+    int lista[] = {10, 15, 3, 7};
+    int lista_size = sizeof(lista) / sizeof(lista[0]); //é len de uma lista em C++
+    bool resposta = false;
+
+    for (int i = 0; i < lista_size; i++){
+        for (int j = i + 1; j < lista_size; j++){
+            int soma = lista[i] + lista[j];
+            if (soma == n){
+                resposta = true;
+                break;
+            }
+        
+        if (resposta){ //porque está ainda dentro de outra lista
+            break;
+        }
+        }
+    }
+
+    if (resposta){
+       std::cout << "Par achado" << std::endl; //se for true
+    }
+    else{
+        std::cout << "Par não achado" << std::endl;  //se for falso
+    }
+    
+    return 0;
+}
