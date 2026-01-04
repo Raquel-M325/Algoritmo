0fc1f6e87bd48a40a4b56ead91ca0037d11c8876
Raquel-M325 <raquelmartiniano14@gmail.com>
2026-01-04 18:39:32 -0300

    não está pronto

--- Atividade 6/ex03.cpp
+++ Atividade 6/ex03.cpp
@@ -11,16 +11,45 @@ void fazer_matriz(int lab[20][20], int coluna, int linha){
 }
 
 int labirinto(int lab[20][20], int linha, int coluna, int linha_atual, int coluna_atual){
-    if (linha_atual < 0 || coluna_atual < 0 || linha_atual >= linha || coluna_atual > = coluna || lab[linha_atual][coluna_atual] !== 0){
-        return 0;
+    if (linha_atual < 0 || coluna_atual < 0 || linha_atual >= linha || coluna_atual >= coluna || lab[linha_atual][coluna_atual] != 0){
+        return -1;
     }
 
     if (linha_atual == linha - 1 && coluna_atual == coluna - 1){
         return 1;
     }
 
-    
+    lab[linha_atual][coluna_atual] = 2;
 
+    bool resposta = false;
+    int contagem = 0;
+    resposta = labirinto(lab, linha, coluna, linha_atual + 1, coluna_atual);
+    if (resposta == false){
+        resposta = labirinto(lab, linha, coluna, linha_atual, coluna_atual + 1);
+        contagem += 0;
+    } else {
+        contagem += 1;
+    }
+
+    if (resposta == false){
+        resposta = labirinto(lab, linha, coluna, linha_atual - 1, coluna_atual);
+        contagem += 0;
+    } else {
+        contagem += 1;
+    }
+
+    if (resposta == false){
+        resposta = labirinto(lab, linha, coluna, linha_atual, coluna_atual - 1);
+        contagem += 0;
+    } else {
+        contagem += 1;
+    }
+
+    if (resposta == false){
+        return -1;
+    }
+
+    return contagem;
 }
 
 int main(){
@@ -39,8 +68,8 @@ int main(){
 
     int coluna_atual = 0, linha_atual = 0;
 
-    fazer_matriz(int lab, int linha, int coluna);
+    fazer_matriz(lab, linha, coluna);
 
-    cout << labirinto(int lab, int linha, int coluna, int coluna_atual, int linha_atual) << endl;
+    cout << labirinto(lab, linha, coluna, coluna_atual, linha_atual) << endl;
     return 0;
 }
